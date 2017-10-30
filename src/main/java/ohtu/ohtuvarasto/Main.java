57284a0127f2796e454442fa6fda275c244fd5a1
techstalgist <mikko.i.ruuskanen@gmail.com>
2017-10-30 10:25:20 +0200

    pieni muutos travikselle

--- src/main/java/ohtu/ohtuvarasto/Main.java
+++ src/main/java/ohtu/ohtuvarasto/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
 
-        System.out.println("Luonnin jälkeen:");
+        System.out.println("Luonnin jälkeen: ");
         System.out.println("Mehuvarasto: " + mehua);
         System.out.println("Olutvarasto: " + olutta);
 
